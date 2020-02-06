74ba715b3b17272d3ec6444964b52f1cf634d6ea
KienanBachwa <kienan.bachwa@etu.unice.fr>
2020-02-06 09:55:10 +0100

    Added data in state for Detail

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -2,16 +2,27 @@ import React, {Component} from 'react';
 
 import './Detail.css';
 import ZoomImg from "./ZoomImg";
-import {Button, Col, Row, Tabs} from "antd";
+import {Button, Col, Descriptions, Row, Tabs} from "antd";
 import ShopCard from "./ShopCard";
-const { TabPane } = Tabs;
+
+const {TabPane} = Tabs;
 type MyProps = {};
-type MyState = { url: string };
+type MyState = {
+    url: string,
+    price: number,
+    name: string,
+    description: string,
+    category: string
+};
 
 class Detail extends Component<MyProps, MyState> {
     constructor(props: any) {
         super(props);
         this.state = {
+            price: 5,
+            name: "UnModule",
+            description: "UneDescription",
+            category: "Synthetizers",
             url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png'
         };
     }
@@ -24,17 +35,20 @@ class Detail extends Component<MyProps, MyState> {
                         <ZoomImg/>
                     </Col>
                     <Col span={18}>
-                        <h1>Titre</h1>
-                        <p>Price</p>
-                        <p>Description</p>
+                        <h1>{this.state.name}</h1>
+                        <Descriptions>
+                            <Descriptions.Item label={"Price"}>{this.state.price}</Descriptions.Item>
+                            <Descriptions.Item label={"Description"}>{this.state.description}</Descriptions.Item>
+                            <Descriptions.Item label={"Category"}>{this.state.category}</Descriptions.Item>
+                        </Descriptions>
                         <Button>Add to Cart</Button>
-                        <p>Category: truc</p>
                     </Col>
                 </Row>
 
                 <Row>
                     <Tabs>
                         <TabPane tab={"Description"} key={"Description"}>
+                            <p>{this.state.description}</p>
                             <Button>Try truc</Button>
                         </TabPane>
                         <TabPane tab={"Infos"} key={"Infos"}>
