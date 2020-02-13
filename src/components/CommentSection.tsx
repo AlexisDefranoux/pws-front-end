4a7957c9e33cd90a050cb3031b93db2afc7f65a1
KienanBachwa <kienan.bachwa@etu.unice.fr>
2020-02-13 12:00:37 +0100

    Added comment input

--- src/components/CommentSection.tsx
+++ src/components/CommentSection.tsx
@@ -1,55 +1,100 @@
-import {Comment, List, Tooltip} from 'antd';
+import {Button, Comment, List, Tooltip} from 'antd';
 import moment from 'moment';
 import React, {Component} from "react";
+import Form from 'antd/es/form';
+import TextArea from 'antd/lib/input/TextArea';
 
 type MyProps = {};
-type MyState = {};
+type MyState = {
+    comments: any[],
+    submitting: boolean,
+    newcommentvalue: string
+};
 
 class CommentSection extends Component<MyProps, MyState> {
 
-    data = [
-        {
-            author: 'Han Solo',
-            avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-            content:
-                "We supply a series of design principles, practical patterns and high quality design" +
-                "resources (Sketch and Axure), to help people create their product prototypes beautifully and" +
-                "efficiently."
-            ,
-            datetime: (
-                moment()
-                    .subtract(1, 'days')
-                    .fromNow()
-            ),
-            datetimestring: (moment()
-                    .subtract(1, 'days')
-                    .format('YYYY-MM-DD HH:mm:ss')
-            )
-        },
-        {
-            author: 'Obi wan',
-            avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-            content:
-                "YES OF COURSE"
-            ,
-            datetime: (
-                moment()
-                    .subtract(3, 'days')
-                    .fromNow()
-            ),
-            datetimestring: (moment()
-                    .subtract(3, 'days')
-                    .format('YYYY-MM-DD HH:mm:ss')
-            )
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            comments: [],
+            submitting: false,
+            newcommentvalue: '',
         }
-    ];
+    }
+
+
+    componentDidMount(): void {
+        this.setState({
+            comments: this.getComments()
+        });
+    }
+
+    getComments() {
+        // sera plus tard chargé depuis le serveur
+
+        return [
+            {
+                author: 'Han Solo',
+                avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+                content:
+                    "We supply a series of design principles, practical patterns and high quality design" +
+                    "resources (Sketch and Axure), to help people create their product prototypes beautifully and" +
+                    "efficiently."
+                ,
+                datetime: (
+                    moment()
+                        .subtract(1, 'days')
+                        .fromNow()
+                ),
+                datetimestring: (moment()
+                        .subtract(1, 'days')
+                        .format('YYYY-MM-DD HH:mm:ss')
+                )
+            },
+            {
+                author: 'Obi wan',
+                avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+                content:
+                    "YES OF COURSE"
+                ,
+                datetime: (
+                    moment()
+                        .subtract(3, 'days')
+                        .fromNow()
+                ),
+                datetimestring: (moment()
+                        .subtract(3, 'days')
+                        .format('YYYY-MM-DD HH:mm:ss')
+                )
+            }
+        ];
+    }
+
+    handleSubmit = () => {
+        if (!this.state.newcommentvalue) {
+            return;
+        }
+
+        this.setState({
+            submitting: true,
+        });
+
+        //TODO Envoyer commentaire serveur et rafraichir section commntaires
+    };
+
+    handleChange = (e: { target: { value: string; }; }) => {
+        this.setState({
+            newcommentvalue: e.target.value,
+        });
+    };
 
     render() {
-        return (<div className="commentSection">
+        return (
+            <div className="commentSection">
                 <List
-                    header={`${this.data.length} comments`}
+                    header={`${this.state.comments.length} comments`}
                     itemLayout="horizontal"
-                    dataSource={this.data}
+                    dataSource={this.state.comments}
                     renderItem={item => (
                         <li>
                             <Comment
@@ -67,6 +112,15 @@ class CommentSection extends Component<MyProps, MyState> {
                         </li>
                     )}
                 />
+                <Form.Item>
+                    <TextArea rows={4} onChange={this.handleChange} value={this.state.newcommentvalue}/>
+                </Form.Item>
+                <Form.Item>
+                    <Button htmlType="submit" loading={this.state.submitting} onClick={this.handleSubmit}
+                            type="primary">
+                        Add Comment
+                    </Button>
+                </Form.Item>
             </div>
         );
     }
